8eacbf2d3770aaa8e9137a8a0899a2cfde722ec1
Stephen Jones <stephen@jonesing.net>
2022-12-28 16:08:15 -0600

    add some ship stats

--- src/game.rs
+++ src/game.rs
@@ -3,21 +3,38 @@
 use bevy_ecs::prelude::*;
 
 // COMPONENTS
-#[derive(Debug, Component)]
+#[derive(Component)]
 struct Position {
     x: u32,
     y: u32,
 }
 
+#[derive(Component)]
+struct BaseShipStats<'a> {
+    name: &'a str,
+    max_shields: u32,
+    max_holds: u32,
+    max_hull: u32,
+    scanner_range: u32,
+}
+
+#[derive(Component)]
+struct ActiveShipData {
+    current_holds_empty: u32,
+    current_holds_fuel: f32,
+    current_hull: u32,
+    current_shields: u32,
+}
+
 #[derive(Debug, Component)]
-struct Name(String);
+struct ShipName(String);
 
 // SYSTEMS
-fn print_positions(query: Query<(Entity, &Position, &Name)>) {
-    for (entity, position, name) in &query {
+fn print_positions(query: Query<(Entity, &Position)>) {
+    for (entity, position) in &query {
         println!(
-            "Entity \"{:?}\" is at position: x {}, y {}",
-            name, position.x, position.y
+            "Entity {:?} is at position: x {}, y {}",
+            entity, position.x, position.y
         );
     }
 }
@@ -25,12 +42,12 @@ fn print_positions(query: Query<(Entity, &Position, &Name)>) {
 pub fn init(world: &mut World, schedule: &mut Schedule) -> std::io::Result<()> {
     world.spawn((
         Position { x: 1, y: 1 },
-        Name(String::from("Star Destroyer 1")),
+        ShipName(String::from("Star Destroyer 1")),
     ));
 
     world.spawn((
         Position { x: 2, y: 2 },
-        Name(String::from("Star Destroyer 2")),
+        ShipName(String::from("Star Destroyer 2")),
     ));
 
     #[derive(StageLabel)]
